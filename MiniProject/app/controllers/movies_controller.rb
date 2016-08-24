052e5545acc3aa3d70d233d814f6c3f5cbc4bd99
pamantonow <unstyleurbain@gmail.com>
2016-08-23 21:20:35 -0600

    need to finish

--- MiniProject/app/controllers/movies_controller.rb
+++ MiniProject/app/controllers/movies_controller.rb
@@ -2,6 +2,7 @@ class MoviesController < ApplicationController
 
 	 def index 
 		@movies = Movie.search((params[:q].present? ? params[:q] : '*')).records
+		render :partial => 'movies/index', locals: {movies: @movies}
 	end
 
 	
